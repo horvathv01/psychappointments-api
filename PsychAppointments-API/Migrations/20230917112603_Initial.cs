2e2a334959d7d239d07babd2fdf7d4a0f2bc65bb
horvathv01 <horvathv01@gmail.com>
2023-09-17 15:09:14 +0200

    rewrite userService

---
+++ PsychAppointments-API/Migrations/20230917112603_Initial.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace PsychAppointments_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Address",
+                columns: table => new
+                {
+                    Country = table.Column<string>(type: "text", nullable: false),
+                    Zip = table.Column<string>(type: "text", nullable: false),
+                    City = table.Column<string>(type: "text", nullable: false),
+                    Street = table.Column<string>(type: "text", nullable: false),
+                    Rest = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Locations",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Locations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Type = table.Column<int>(type: "integer", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Phone = table.Column<string>(type: "text", nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    RegisteredById = table.Column<long>(type: "bigint", nullable: false),
+                    Discriminator = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_User_User_RegisteredById",
+                        column: x => x.RegisteredById,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LocationManagers",
+                columns: table => new
+                {
+                    LocationsId = table.Column<long>(type: "bigint", nullable: false),
+                    ManagersId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LocationManagers", x => new { x.LocationsId, x.ManagersId });
+                    table.ForeignKey(
+                        name: "FK_LocationManagers_Locations_LocationsId",
+                        column: x => x.LocationsId,
+                        principalTable: "Locations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_LocationManagers_User_ManagersId",
+                        column: x => x.ManagersId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LocationPsychologists",
+                columns: table => new
+                {
+                    LocationId = table.Column<long>(type: "bigint", nullable: false),
+                    PsychologistsId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LocationPsychologists", x => new { x.LocationId, x.PsychologistsId });
+                    table.ForeignKey(
+                        name: "FK_LocationPsychologists_Locations_LocationId",
+                        column: x => x.LocationId,
+                        principalTable: "Locations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_LocationPsychologists_User_PsychologistsId",
+                        column: x => x.PsychologistsId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PsychologistClients",
+                columns: table => new
+                {
+                    ClientsId = table.Column<long>(type: "bigint", nullable: false),
+                    PsychologistsId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PsychologistClients", x => new { x.ClientsId, x.PsychologistsId });
+                    table.ForeignKey(
+                        name: "FK_PsychologistClients_User_ClientsId",
+                        column: x => x.ClientsId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PsychologistClients_User_PsychologistsId",
+                        column: x => x.PsychologistsId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Slots",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    PsychologistId = table.Column<long>(type: "bigint", nullable: false),
+                    LocationId = table.Column<long>(type: "bigint", nullable: false),
+                    Date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    SlotStart = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    SlotEnd = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    SessionLength = table.Column<int>(type: "integer", nullable: false),
+                    Rest = table.Column<int>(type: "integer", nullable: false),
+                    Weekly = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Slots", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Slots_Locations_LocationId",
+                        column: x => x.LocationId,
+                        principalTable: "Locations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Slots_User_PsychologistId",
+                        column: x => x.PsychologistId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sessions",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    PsychologistId = table.Column<long>(type: "bigint", nullable: false),
+                    PartnerPsychologistId = table.Column<long>(type: "bigint", nullable: false),
+                    Blank = table.Column<bool>(type: "boolean", nullable: false),
+                    LocationId = table.Column<long>(type: "bigint", nullable: false),
+                    Date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Start = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    End = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ClientId = table.Column<long>(type: "bigint", nullable: false),
+                    Price = table.Column<int>(type: "integer", nullable: false),
+                    Frequency = table.Column<int>(type: "integer", nullable: false),
+                    SlotId = table.Column<long>(type: "bigint", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sessions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Sessions_Locations_LocationId",
+                        column: x => x.LocationId,
+                        principalTable: "Locations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Sessions_Slots_SlotId",
+                        column: x => x.SlotId,
+                        principalTable: "Slots",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Sessions_User_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Sessions_User_PartnerPsychologistId",
+                        column: x => x.PartnerPsychologistId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Sessions_User_PsychologistId",
+                        column: x => x.PsychologistId,
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LocationManagers_ManagersId",
+                table: "LocationManagers",
+                column: "ManagersId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LocationPsychologists_PsychologistsId",
+                table: "LocationPsychologists",
+                column: "PsychologistsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PsychologistClients_PsychologistsId",
+                table: "PsychologistClients",
+                column: "PsychologistsId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sessions_ClientId",
+                table: "Sessions",
+                column: "ClientId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sessions_LocationId",
+                table: "Sessions",
+                column: "LocationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sessions_PartnerPsychologistId",
+                table: "Sessions",
+                column: "PartnerPsychologistId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sessions_PsychologistId",
+                table: "Sessions",
+                column: "PsychologistId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sessions_SlotId",
+                table: "Sessions",
+                column: "SlotId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Slots_LocationId",
+                table: "Slots",
+                column: "LocationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Slots_PsychologistId",
+                table: "Slots",
+                column: "PsychologistId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_RegisteredById",
+                table: "User",
+                column: "RegisteredById");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Address");
+
+            migrationBuilder.DropTable(
+                name: "LocationManagers");
+
+            migrationBuilder.DropTable(
+                name: "LocationPsychologists");
+
+            migrationBuilder.DropTable(
+                name: "PsychologistClients");
+
+            migrationBuilder.DropTable(
+                name: "Sessions");
+
+            migrationBuilder.DropTable(
+                name: "Slots");
+
+            migrationBuilder.DropTable(
+                name: "Locations");
+
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
